391c9cbd0b6aa178c52819fa3e9c33836f9d7000
abhijithravuri <37011997+abhijithravuri@users.noreply.github.com>
2020-12-16 00:06:52 +0530

    final commit

--- script.js
+++ script.js
@@ -3,49 +3,56 @@
 /**
  * Update the output text as you type in the textarea
  */
-function updateText(){
-  let text = document.getElementById("text-input").value;
+updateText = () => {
+  let text = document.getElementById('text-input').value;
   document.getElementById('text-output').innerText = text;
-}
+};
 
 /**
  * Toggle the bold class for the output text
  */
-function makeBold(elem){
+makeBold = (elem) => {
   elem.classList.toggle('active');
   document.getElementById('text-output').classList.toggle('bold');
-}
+};
 
 /**
  * Toggle the italic class for the output text
  */
-function makeItalic(elem){
+makeItalic = (elem) => {
   elem.classList.toggle('active');
   document.getElementById('text-output').classList.toggle('italic');
-}
+};
 
 /**
  * Toggle the underline class for the output text
  */
-function makeUnderline(elem){
-    elem.classList.toggle('active');
-    let output = document.getElementById('text-output');
-    if(output.classList.contains('underline')){
-      output.classList.remove('underline');
-    } else {
-      output.classList.add('underline');
-    }
-}
+makeUnderline = (elem) => {
+  elem.classList.toggle('active');
+  let output = document.getElementById('text-output');
+  if (output.classList.contains('underline')) {
+    output.classList.remove('underline');
+  } else {
+    output.classList.add('underline');
+  }
+};
 
 /**
  * Toggle the style textAlign attribute
  * Toggle the active state for the align butttons
  */
-function alignText(elem, alignType){
+alignText = (elem, alignType) => {
   document.getElementById('text-output').style.textAlign = alignType;
   let alignButtons = document.getElementsByClassName('align');
-  for(let i = 0; i < alignButtons.length; i++ ){
-    alignButtons[i].classList.remove('active');
+  for (let button of alignButtons) {
+    button.classList.remove('active');
+    console.log(button);
   }
+  // alignButtons.map((button) => {
+  //   button.classList.remove('active');
+  // });
+  // for (let i = 0; i < alignButtons.length; i++) {
+  //   alignButtons[i].classList.remove('active');
+  // }
   elem.classList.toggle('active');
-}
+};
